b2826f5976580b3cef2a8f49cdb8059a54318d07
aseem sethi <aseemsethi@yahoo.com>
2022-07-21 21:46:49 +0530

    IOS changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,22 @@ import '../utils/mqttAppState.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'dart:isolate';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   name: 'my_app',
+  //   options: const FirebaseOptions(
+  //     apiKey: "AIzaSyBSlG5gwt8BNYgTWSoykgYkFK1wnZ3AaOw",
+  //     appId: "1:654349620102:android:2e90a5c7ee477784401207",
+  //     messagingSenderId: "654349620102",
+  //     projectId: "flutterapp-b1ba6",
+  //   ),
+  // );
   await Firebase.initializeApp(
-    name: 'my_app',
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyBSlG5gwt8BNYgTWSoykgYkFK1wnZ3AaOw",
-      appId: "1:654349620102:android:2e90a5c7ee477784401207",
-      messagingSenderId: "654349620102",
-      projectId: "flutterapp-b1ba6",
-    ),
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   // runApp(MyApp());
   runApp(
