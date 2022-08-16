0073da41fc56f0688b6d9c4117b460bb6715f9a6
Aseem Sethi <aseemsethi@yahoo.com>
2022-08-16 09:13:24 +0530

    Tried to use geoloc for IOS...but that does not keep ios alive either.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/screens/welcome_page.dart';
+//import 'package:my_app/screens/welcome_page.dart';
 import 'package:my_app/utils/db_helper.dart';
 import 'navigation/navigate.dart';
 import 'utils/constants.dart';
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_nsd/flutter_nsd_plugin.h>
+#include <geolocator_windows/geolocator_windows.h>
 #include <network_info_plus_windows/network_info_plus_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterNsdPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterNsdPlugin"));
+  GeolocatorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GeolocatorWindows"));
   NetworkInfoPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("NetworkInfoPlusWindowsPlugin"));
 }
